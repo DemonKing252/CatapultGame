8581a558c23717e4fecf722f23bb7f10c7a8efd6
Bojarzin <47534935+Bojarzin@users.noreply.github.com>
2022-06-15 13:02:08 -0400

    Added a camera controller and level boundary prefab
    The camera will follow the ball, but will keep an offset based on the level boundary that is set via a Rect Transform in each level

---
+++ Assets/[Scripts]/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public RectTransform levelBoundary;
+    public Transform followTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /// Log for the purposes of checking if the boundary limits are working
+        //Debug.Log("Boundary -X: " + (levelBoundary.position.x - levelBoundary.rect.width / 2 + (((float)Camera.main.orthographicSize) * Camera.main.aspect)) + "\n" +
+        //          "Boundary +X: " + (levelBoundary.position.x + levelBoundary.rect.width / 2 - (((float)Camera.main.orthographicSize) * Camera.main.aspect)) + "\n" +
+        //          "Boundary -Y: " + (levelBoundary.position.y - levelBoundary.rect.height / 2 + ((float)Camera.main.orthographicSize)) + "\n" +
+        //          "Boundary +Y: " + (levelBoundary.position.y + levelBoundary.rect.height / 2 - ((float)Camera.main.orthographicSize)) + "\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClampCameraPosition();
+
+        /// Checking camera position to match with the boundary logs
+        //Debug.Log("Camera Position X: " + transform.position.x + "\n" +
+        //          "Camera Position Y: " + transform.position.y);
+    }
+
+    void ClampCameraPosition()
+    {
+        // The position of the camera will be based on the positon of the ball falling within the preset boundary of the given level.
+        // It will grab the width and height of the levelBoundary variable in the level then will offset the camera width and height
+        transform.position = new Vector3(Mathf.Clamp(followTarget.position.x, levelBoundary.position.x - levelBoundary.rect.width / 2 + (((float)Camera.main.orthographicSize) * Camera.main.aspect),
+                                                                              levelBoundary.position.x + levelBoundary.rect.width / 2 - (((float)Camera.main.orthographicSize) * Camera.main.aspect)),
+                                         Mathf.Clamp(followTarget.position.y, levelBoundary.position.y - levelBoundary.rect.height / 2 + ((float)Camera.main.orthographicSize),
+                                                                              levelBoundary.position.y + levelBoundary.rect.height / 2 - ((float)Camera.main.orthographicSize)),
+                                                        transform.position.z);
+
+    }
+}
